080281c4f3b2c012a44d8b1a70eae443bba9a582
procodersidd <149250903+procodersidd@users.noreply.github.com>
2023-11-18 19:43:07 +0530

    Update predict.py

--- predict.py
+++ predict.py
@@ -28,3 +28,33 @@ def prediction(car_df, car_width, engine_size, horse_power, drive_wheel_fwd, car
     test_rmse = np.sqrt(mean_squared_error(y_test, y_test_pred))
 
     return price, score, test_r2_score, test_mae, test_msle, test_rmse
+# S2.2: Define the 'app()' function as directed above.
+def app(car_df):
+    st.markdown("<p style='color:blue;font-size:25px'>This app uses <b>Linear regression</b> to predict the price of a car based on your inputs.", unsafe_allow_html = True)
+    st.subheader("Select Values:")
+    car_wid = st.slider("Car Width", float(car_df["carwidth"].min()), float(car_df["carwidth"].max()))
+    eng_siz = st.slider("Engine Size", int(car_df["enginesize"].min()), int(car_df["enginesize"].max()))
+    hor_pow = st.slider("Horse Power", int(car_df["horsepower"].min()), int(car_df["horsepower"].max()))
+    drw_fwd = st.radio("Is it a forward drive wheel car?", ("Yes", "No"))
+    if drw_fwd == 'No':
+        drw_fwd = 0
+    else:
+        drw_fwd = 1
+    com_bui = st.radio("Is the car manufactured by Buick?", ("Yes", "No"))
+    if com_bui == 'No':
+        com_bui = 0
+    else:
+        com_bui = 1
+
+    # When 'Predict' button is clicked, the 'prediction()' function must be called
+    # and the value returned by it must be stored in a variable, say 'price'.
+    # Print the value of 'price' and 'score' variable using the 'st.success()' and 'st.info()' functions respectively.
+    if st.button("Predict"):
+        st.subheader("Prediction results:")
+        price, score, car_r2, car_mae, car_msle, car_rmse = prediction(car_df, car_wid, eng_siz, hor_pow, drw_fwd, com_bui)
+        st.success("The predicted price of the car: ${:,}".format(int(price)))
+        st.info("Accuracy score of this model is: {:2.2%}".format(score))
+        st.info(f"R-squared score of this model is: {car_r2:.3f}")
+        st.info(f"Mean absolute error of this model is: {car_mae:.3f}")
+        st.info(f"Mean squared log error of this model is: {car_msle:.3f}")
+        st.info(f"Root mean squared error of this model is: {car_rmse:.3f}")
