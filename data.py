4d52faadc388f8473448bcb97b2f2fe93aa7e2b0
procodersidd <149250903+procodersidd@users.noreply.github.com>
2023-11-18 19:36:37 +0530

    Update data.py

--- data.py
+++ data.py
@@ -1,3 +1,5 @@
+# Code for 'data.py' file
+# Import necessary modules
 import numpy as np
 import pandas as pd
 import streamlit as st
@@ -9,7 +11,24 @@ def app(car_df):
     with st.beta_expander("View Dataset"):
         st.table(car_df)
 
-    # ADD NEW CODE HERE.
     st.subheader("Columns Description:")
     if st.checkbox("Show summary"):
         st.table(car_df.describe())
+
+    beta_col1, beta_col2, beta_col3 = st.beta_columns(3)
+
+    # Add a checkbox in the first column. Display the column names of 'car_df' on the click of checkbox.
+    with beta_col1:
+        if st.checkbox("Show all column names"):
+            st.table(list(car_df.columns))
+
+    # Add a checkbox in the second column. Display the column data-types of 'car_df' on the click of checkbox.
+    with beta_col2:
+        if st.checkbox("View column data-type"):
+            st.table(car_df.dtypes)
+
+    # Add a checkbox in the third column followed by a selectbox which accepts the column name whose data needs to be displayed.
+    with beta_col3:
+        if st.checkbox("View column data"):
+            column_data = st.selectbox('Select column', tuple(car_df.columns))
+            st.write(car_df[column_data])
